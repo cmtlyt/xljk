eb6623b677ce265c1ce51390946ae4256ba49ef3
cmtlyt <cmtlyt@163.com>
2022-10-11 17:00:28 +0800

    20221011-(家长端)-教师详情样式修改

--- src/views/parent/index/booking/TeacherDesc.vue
+++ src/views/parent/index/booking/TeacherDesc.vue
@@ -343,7 +343,6 @@ export default {
       div {
         display: flex;
         flex-direction: column;
-        // justify-content: space-between;
         gap: 1.5rem;
         align-items: center;
         font-size: $title-font-size;
@@ -543,6 +542,7 @@ export default {
     .van-goods-action {
       padding: 0 1rem 0 3rem;
       gap: 10px;
+      z-index: 100;
     }
   }
 }
