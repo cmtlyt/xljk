32ceb33cc41a98803d2ce0ac4e8c561fe1d18636
cmtlyt <cmtlyt@163.com>
2022-10-11 17:21:40 +0800

    20221011-(家长端)-深度好文导航添加划动功能

--- src/views/parent/book/Index.vue
+++ src/views/parent/book/Index.vue
@@ -10,7 +10,7 @@
       </template>
     </cmtlyt-navbar>
     <div class="content">
-      <van-tabs v-model="activeTab" color="#0283fe">
+      <van-tabs v-model="activeTab" color="#0283fe" swipeable>
         <van-tab
           v-for="(item, idx) in tabList"
           :key="idx"
