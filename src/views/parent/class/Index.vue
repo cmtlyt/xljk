da1d99dba77bd108a94cd845574dd333f8083110
cmtlyt <cmtlyt@163.com>
2022-10-11 08:33:36 +0800

    20221011-(家长端)-在线课程

--- src/views/parent/class/Index.vue
+++ src/views/parent/class/Index.vue
@@ -1,14 +1,197 @@
 <template>
-  <div class="class"></div>
+  <div class="class">
+    <cmtlyt-navbar :role="navbarRole">
+      <template #right>
+        <van-icon
+          name="chat"
+          :badge="messageTotal ?? ''"
+          @click="$goto('/messageList')"
+        />
+      </template>
+    </cmtlyt-navbar>
+    <header>
+      <van-swipe :autoplay="3000">
+        <van-swipe-item v-for="(item, idx) in images" :key="idx">
+          <img :src="item" />
+        </van-swipe-item>
+      </van-swipe>
+    </header>
+    <div class="content">
+      <van-tabs v-model="activeTab" color="#0283fe">
+        <van-tab
+          v-for="(item, idx) in tabList"
+          :key="idx"
+          :name="item.name"
+          :title="item.label || item.name"
+        >
+          <class-item
+            v-for="(role, idx) in item.list"
+            :key="idx"
+            :role="role"
+          ></class-item>
+        </van-tab>
+      </van-tabs>
+    </div>
+  </div>
 </template>
 
 <script>
 import CmtlytNavbar from "@/components/Navbar.vue";
+import ClassItem from "./components/Item.vue";
+import { Icon, Tabs, Tab, Swipe, SwipeItem } from "vant";
 export default {
-  components: {CmtlytNavbar},
+  components: {
+    CmtlytNavbar,
+    [Icon.name]: Icon,
+    [Tabs.name]: Tabs,
+    [Tab.name]: Tab,
+    [Swipe.name]: Swipe,
+    [SwipeItem.name]: SwipeItem,
+    ClassItem,
+  },
   props: [],
   data() {
-    return {};
+    return {
+      navbarRole: { type: "search" },
+      messageTotal: "3",
+      activeTab: "",
+      tabList: [
+        {
+          name: "人际关系",
+          label: "",
+          list: [
+            {
+              title: "到底什么样的关系,才算是健康的人际关系?",
+              author: "张三",
+              readTotal: "1211",
+            },
+            {
+              title: "到底什么样的关系,才算是健康的人际关系?",
+              author: "张三",
+              readTotal: "1211",
+            },
+            {
+              title: "到底什么样的关系,才算是健康的人际关系?",
+              author: "张三",
+              readTotal: "1211",
+            },
+            {
+              title: "到底什么样的关系,才算是健康的人际关系?",
+              author: "张三",
+              readTotal: "1211",
+            },
+          ],
+        },
+        {
+          name: "心理解读",
+          label: "",
+          list: [
+            {
+              title: "到底什么样的关系,才算是健康的人际关系?",
+              author: "张三",
+              readTotal: "1211",
+            },
+            {
+              title: "到底什么样的关系,才算是健康的人际关系?",
+              author: "张三",
+              readTotal: "1211",
+            },
+            {
+              title: "到底什么样的关系,才算是健康的人际关系?",
+              author: "张三",
+              readTotal: "1211",
+            },
+            {
+              title: "到底什么样的关系,才算是健康的人际关系?",
+              author: "张三",
+              readTotal: "1211",
+            },
+          ],
+        },
+        {
+          name: "情绪调解",
+          label: "",
+          list: [
+            {
+              title: "到底什么样的关系,才算是健康的人际关系?",
+              author: "张三",
+              readTotal: "1211",
+            },
+            {
+              title: "到底什么样的关系,才算是健康的人际关系?",
+              author: "张三",
+              readTotal: "1211",
+            },
+            {
+              title: "到底什么样的关系,才算是健康的人际关系?",
+              author: "张三",
+              readTotal: "1211",
+            },
+            {
+              title: "到底什么样的关系,才算是健康的人际关系?",
+              author: "张三",
+              readTotal: "1211",
+            },
+          ],
+        },
+        {
+          name: "个人成长",
+          label: "",
+          list: [
+            {
+              title: "到底什么样的关系,才算是健康的人际关系?",
+              author: "张三",
+              readTotal: "1211",
+            },
+            {
+              title: "到底什么样的关系,才算是健康的人际关系?",
+              author: "张三",
+              readTotal: "1211",
+            },
+            {
+              title: "到底什么样的关系,才算是健康的人际关系?",
+              author: "张三",
+              readTotal: "1211",
+            },
+            {
+              title: "到底什么样的关系,才算是健康的人际关系?",
+              author: "张三",
+              readTotal: "1211",
+            },
+          ],
+        },
+        {
+          name: "学业解剖",
+          label: "",
+          list: [
+            {
+              title: "到底什么样的关系,才算是健康的人际关系?",
+              author: "张三",
+              readTotal: "1211",
+            },
+            {
+              title: "到底什么样的关系,才算是健康的人际关系?",
+              author: "张三",
+              readTotal: "1211",
+            },
+            {
+              title: "到底什么样的关系,才算是健康的人际关系?",
+              author: "张三",
+              readTotal: "1211",
+            },
+            {
+              title: "到底什么样的关系,才算是健康的人际关系?",
+              author: "张三",
+              readTotal: "1211",
+            },
+          ],
+        },
+      ],
+      images: [
+        "https://img01.yzcdn.cn/vant/apple-1.jpg",
+        "https://img01.yzcdn.cn/vant/apple-2.jpg",
+      ],
+    };
   },
   methods: {},
   computed: {},
@@ -18,4 +201,50 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.class {
+  display: flex;
+  flex-direction: column;
+  header {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    .van-swipe {
+      border-radius: $main-radius;
+      .van-swipe-item {
+        img {
+          width: 100%;
+          height: 20rem;
+        }
+      }
+    }
+  }
+  .content {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    .van-tabs {
+      flex: 1;
+      ::v-deep(.van-tabs__nav) {
+        background-color: transparent;
+      }
+      ::v-deep(.van-tab--active) {
+        color: $main-color;
+      }
+      ::v-deep(.van-tabs__line) {
+        bottom: 20px;
+      }
+      ::v-deep(.van-tabs__content) {
+        padding: $main-padding;
+        .van-tab__pane {
+          display: grid;
+          grid-template-columns: repeat(2, 1fr);
+          gap: $main-gap;
+          .class-item {
+            width: 50%;
+          }
+        }
+      }
+    }
+  }
+}
 </style>
---
+++ src/views/parent/class/components/Item.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="history-item" @click="goDesc">
+    <main>
+      <van-image
+        fit="cover"
+        :src="role.src || 'https://img01.yzcdn.cn/vant/cat.jpeg'"
+      />
+      <div class="content">
+        <div class="title">{{ role.title }}</div>
+        <div class="info">
+          <div class="author">{{ role.author }}</div>
+          <div class="readTotal">{{ role.readTotal }}人阅读</div>
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+import { Image } from "vant";
+export default {
+  components: {
+    [Image.name]: Image,
+  },
+  props: ["role"],
+  data() {
+    return {};
+  },
+  methods: {
+    goDesc() {
+      this.$goto("../classDesc");
+    },
+  },
+  computed: {},
+  mounted() {},
+  created() {},
+};
+</script>
+
+<style lang="scss" scoped>
+.history-item {
+  border-radius: $main-radius;
+  overflow: hidden;
+  background-color: #fff;
+  main {
+    display: flex;
+    flex-direction: column;
+    .van-image {
+      flex: 3;
+    }
+    .content {
+      padding: $main-padding;
+      flex: 7;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      gap: $main-gap;
+      .title {
+        display: -webkit-box;
+        -webkit-line-clamp: 1;
+        text-overflow: ellipsis;
+        -webkit-box-orient: vertical;
+        overflow: hidden;
+        height: 1.5em;
+        font-size: $title-font-size;
+        text-align: left;
+      }
+      .info {
+        display: flex;
+        justify-content: space-between;
+        font-size: $desc-font-size;
+        color: #7f7f7f;
+      }
+    }
+  }
+}
+</style>
